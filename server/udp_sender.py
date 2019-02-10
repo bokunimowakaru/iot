2a764b78c4b040684677a97cfa244a50f09f6952
Wataru KUNINO <xbee@dream.jp>
2019-02-10 12:36:35 +0900

    Google AIY Voice Kit用サンプルとUDP送受信用サンプルの追加

---
+++ server/udp_sender.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# UDPを送信する
+# Copyright (c) 2018-2019 Wataru KUNINO
+
+from __future__ import print_function
+import sys
+import socket
+
+argc = len(sys.argv)                                    # 引数の数をargcへ代入
+print('UDP Sender (usage: '+sys.argv[0]+' port < data)')# タイトル表示
+if argc == 2:                                           # 入力パラメータ数の確認
+    port = sys.argv[1]                                  # ポート番号を設定
+    if port < 1 or port > 65535:                        # ポート1未満or65535超の時
+        port = 1024                                     # UDPポート番号を1024に
+else:
+    port = 1024
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # ソケットを作成
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+if sock:                                                # 作成に成功したとき
+    for line in sys.stdin:                              # 標準入力から変数lineへ
+        udp=''                                          # 表示用の文字列変数udp
+        for c in line:                                  # 入力行内
+            if ord(c) >= ord(' '):                      # 表示可能文字
+                udp += c                                # 文字列strへ追加
+        print('send : ' + udp)                          # 受信データを出力
+        udp=(udp + '\n').encode()                       # バイト列に変換
+        sock.sendto(udp,('255.255.255.255',port))       # UDPブロードキャスト送信
+    sock.close()                                        # ソケットの切断
---
+++ voice/googleAiySpeechToUdp.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Google AIY Voice Kit用
+# 音声「LEDをON」や「LEDをOFF」でUDPを送信する
+# Copyright (c) 2019 Wataru KUNINO
+
+from aiy.cloudspeech import CloudSpeechClient               # AIY音声認識用ライブラリ
+from aiy.board import Board,Led                             # AIYキット用ライブラリ
+import socket                                               # UDP送信用ライブラリ
+
+device = 'voice_1'                                          # UDP送信用のデバイス名
+port = 1024                                                 # UDPポート番号
+
+vclient = CloudSpeechClient()                               # 音声認識用 vclient の生成
+board = Board()                                             # AIYキット用 board の生成
+board.led.state = Led.BLINK                                 # LEDを点滅させる
+print('準備完了')                                           # 準備完了と表示する
+
+while True:
+    udp = None                                              # 送信データ用変数udpを定義
+    voice = vclient.recognize(language_code='ja_JP')        # 日本語による音声認識
+    if voice == None:                                       # 音声データが無かったとき
+        continue                                            # whileループの先頭に戻る
+    print('認識結果 =',voice)                               # 認識結果を表示する
+    if '終了' in voice:                                     # 音声「終了」を認識したとき
+        break                                               # whileループを抜ける
+    if 'LED' in voice.upper():                              # 音声「LED」を認識したとき
+        board.led.state = Led.BLINK                         # LEDを点滅させる
+        if 'オフ' in voice or '4' in voice:                 # かつ「OFF」を認識したとき
+            board.led.state = Led.OFF                       # LEDをOFFする
+            udp = device + ', 0'                            # 変数udpへ送信データを代入
+        if 'オン' in voice or '音' in voice or 'ON' in voice.upper():   # 音声「ON」時
+            board.led.state = Led.ON                        # LEDをONする
+            udp = device + ', 1'                            # 変数udpへ送信データを代入
+    if udp == None:                                         # 変数udpがNodeのとき
+        continue                                            # whileループの先頭に戻る
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # ソケットを作成
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)   # ソケット接続を行う
+    if sock:                                                # ソケット接続に成功したとき
+        print('send : ' + udp)                              # 受信データを出力
+        udp=(udp + '\n').encode()                           # バイト列に変換
+        sock.sendto(udp,('255.255.255.255',port))           # UDPブロードキャスト送信
+        sock.close()                                        # ソケットの切断
+board.close()                                               # Google AIYキットのGPIO開放
+exit
