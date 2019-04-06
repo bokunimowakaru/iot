ba9e736d6258f66188f464554b079d3d3c098e23
Wataru KUNINO <xbee@dream.jp>
2019-04-07 02:16:40 +0900

    closeに括弧付与

--- learning/example05_temp.py
+++ learning/example05_temp.py
@@ -5,5 +5,5 @@
 fp = open('/sys/class/thermal/thermal_zone0/temp') # 温度ファイルを開く
 if fp:                                  # ファイルを開くことに成功したとき
     temp = float(fp.read()) / 1000      # ファイルを読み込み1000で除算する
-    fp.close                            # ファイルを閉じる
+    fp.close()                          # ファイルを閉じる
     print('Temperature =',temp)         # 温度を表示する
