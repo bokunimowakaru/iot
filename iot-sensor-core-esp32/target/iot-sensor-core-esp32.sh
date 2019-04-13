29cb4f562c65c3362faef883311674b4d51a587c
Wataru KUNINO <xbee@dream.jp>
2019-04-13 12:21:26 +0900

    最新のestptoolをダウンロードするように変更

--- iot-sensor-core-esp32/target/iot-sensor-core-esp32.sh
+++ iot-sensor-core-esp32/target/iot-sensor-core-esp32.sh
@@ -11,5 +11,12 @@ if [ ! -e ${1} ]; then
 	echo "ERROR: 有効なデバイスPath(/dev/ttyUSB*)を入力してください"
 	exit
 fi
+if [ ! -e "./esptool.py" ]; then
+	echo "esptool.pyをダウンロードします"
+	wget https://raw.githubusercontent.com/espressif/esptool/master/esptool.py
+	chmod a+x esptool.py
+	wget https://raw.githubusercontent.com/espressif/esptool/master/LICENSE
+	\mv -f LICENSE esptool_LICENSE.txt
+fi
 ./esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 iot-sensor-core-esp32.ino.bin 0x8000 iot-sensor-core-esp32.ino.partitions.bin
 echo "Done"
