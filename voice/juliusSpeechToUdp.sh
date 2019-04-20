3975f589a353070387ee198134e5427424d3486a
Wataru KUNINO <xbee@dream.jp>
2019-04-20 18:10:40 +0900

    LSADEV設定の引数入力対応

--- voice/juliusSpeechToUdp.sh
+++ voice/juliusSpeechToUdp.sh
@@ -4,11 +4,16 @@
 julius_path_main_conf="/home/pi/julius/dictation-kit-v4.4/main.jconf"
 julius_path_amgmm_conf="/home/pi/julius/dictation-kit-v4.4/am-gmm.jconf"
 julius_com="/usr/local/bin/julius"
+export ALSADEV="plughw:0,0"
 
-if [ $# -eq 3 ]; then
-	julius_path_main_conf=${1}
-	julius_path_amgmm_conf=${2}
+if [ $# -eq 1 ]; then
+	export ALSADEV="plughw:"${1}",0"
+elif [ $# -eq 3 ]; then
+	export ALSADEV="plughw:"${1}",0"
+	julius_path_main_conf=${2}
+	julius_path_amgmm_conf=${3}
 fi
+echo "ALSADEV =" $ALSADEV
 echo ${julius_com} "-C" ${julius_path_main_conf} "-C" ${julius_path_amgmm_conf} "-quiet"
 ${julius_com} -C ${julius_path_main_conf} -C ${julius_path_amgmm_conf} -quiet | ./juliusSpeechToUdp.py SUBPROCESS
 echo "Done" ${0}
