6eb18ae93f6f8803621ec0db607083724b24f034
Wataru KUNINO <xbee@dream.jp>
2019-05-26 17:43:53 +0900

    温度補正

--- learning/example10_ifttt_temp.py
+++ learning/example10_ifttt_temp.py
@@ -4,13 +4,14 @@
 
 ifttt_token='0123456-012345678ABCDEFGHIJKLMNOPQRSTUVWXYZ'   # ここにTokenを記入
 ifttt_event='notify'                                        # イベント名を記入
-message_s  ='ボタンが押されました'
 
 from sys import argv                                # 本プログラムの引数argvを取得
-from time import sleep
+from time import sleep                              # sleepコマンドを組み込む
 
 import urllib.request                               # HTTP通信ライブラリを組み込む
 import json                                         # JSON変換ライブラリを組み込む
+
+temp_offset = -25.0                                 # 温度補正
 
 filename='/sys/class/thermal/thermal_zone0/temp'    # 温度ファイル
 url_s = 'https://maker.ifttt.com/trigger/'+ifttt_event+'/with/key/'+ifttt_token
@@ -28,30 +29,26 @@ while True:                                         # 永久ループ
         sleep(60)                                   # プログラムの一時停止(60秒)
         continue                                    # whileの先頭に戻る
 
-    temp = float(fp.read()) / 1000                  # ファイルを読み込み1000で除算
+    temp = float(fp.read()) / 1000 + temp_offset    # ファイルを読み込み1000で除算
     fp.close()                                      # ファイルを閉じる
     print('Temperature =',temp)                     # 温度を表示する
 
+    # 温度値に応じた処理
     if temp < 30:                                   # 室温が30℃未満の時
         sleep(5)                                    # プログラムの一時停止(5秒)
         continue                                    # whileの先頭に戻る
         
     # IFTTTへ送信
-                                                    # アクセス先を変数url_sへ代入
     body_dict['value1'] = '室温が' + "{0:.1f}".format(temp) + '度になりました'
     print(head_dict)                                # 送信ヘッダhead_dictを表示
     print(body_dict)                                # 送信内容body_dictを表示
-
     post = urllib.request.Request(url_s, json.dumps(body_dict).encode(), head_dict)
                                                     # POSTリクエストデータを作成
-
     try:                                            # 例外処理の監視を開始
         res = urllib.request.urlopen(post)          # HTTPアクセスを実行
-
     except Exception as e:                          # 例外処理発生時
         print(e,url_s)                              # エラー内容と変数url_sを表示
         exit()                                      # プログラムの終了
-
     res_str = res.read().decode()                   # 受信テキストを変数res_strへ
     res.close()                                     # HTTPアクセスの終了
     print('Response:', res_str)                     # 変数res_strの内容を表示
