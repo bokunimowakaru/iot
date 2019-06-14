5a01ca3b36c8826888bbd038eb03a8cd7aeae637
Wataru KUNINO <xbee@dream.jp>
2019-06-14 18:05:10 +0900

    udpの受信後処理を分割

--- learning/example16_udp_logger.py
+++ learning/example16_udp_logger.py
@@ -14,7 +14,9 @@ except Exception as e:                                  # 例外処理発生時
     print(e)                                            # エラー内容を表示
     exit()                                              # プログラムの終了
 while sock:                                             # 永遠に繰り返す
-    udp = sock.recv(64).decode().strip()                # UDPパケットを取得
+    udp = sock.recv(64)                                 # UDPパケットを取得
+    udp = udp.decode()                                  # 文字列へ変換する
+    udp = udp.strip()                                   # 先頭末尾の改行削除
     if udp.isprintable():                               # 全文字が表示可能
         date=datetime.datetime.today()                  # 日付を取得
         print(date.strftime('%Y/%m/%d %H:%M'), end='')  # 日付を出力
