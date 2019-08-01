5f80338f5c3073425d73717b5774b361657b2b54
Wataru KUNINO <xbee@dream.jp>
2019-08-01 19:55:16 +0900

    MicroPythonダウンロードツール

---
+++ micropython/esp32/download.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "ESP32 へ MicroPython を書き込みます (usage: ${0} port)"
+if [ ${#} -ne 1 ]; then
+	cat README.md
+	echo "ERROR: シリアルポートのデバイスPath(/dev/ttyUSB*)を入力してください"
+	exit
+fi
+if [ ! -e ${1} ]; then
+	cat README.md
+	echo "ERROR: 有効なデバイスPath(/dev/ttyUSB*)を入力してください"
+	exit
+fi
+if [ ! -e "./esptool.py" ]; then
+	echo "esptool.pyをダウンロードします"
+	wget https://raw.githubusercontent.com/espressif/esptool/master/esptool.py
+	chmod a+x esptool.py
+	wget https://raw.githubusercontent.com/espressif/esptool/master/LICENSE
+	\mv -f LICENSE esptool_LICENSE.txt
+fi
+
+filename="esp32-20190529-v1.11.bin"
+if [ ! -e ${filename} ]; then
+	echo ${filename} "をダウンロードします"
+	wget http://micropython.org/resources/firmware/esp32-20190529-v1.11.bin
+	wget https://github.com/micropython/micropython/blob/master/LICENSE
+	\mv -f LICENSE micropython_LICENSE.txt
+fi
+
+echo "ESP32を初期化します。"
+./esptool.py --chip esp32 --port /dev/ttyUSB0 erase_flash
+if [ $? -ne 0 ]; then
+	cat README.md
+	echo "初期化に失敗しました。"
+	exit
+fi
+echo "ESP32へ書き込みます"
+./esptool.py --chip esp32 --port ${1} --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 ${filename}
+if [ $? -ne 0 ]; then
+	cat README.md
+	echo "書き込みに失敗しました。"
+	exit
+fi
+echo "ESP32への書き込みが完了しました。"
+echo "Done"
