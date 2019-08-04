d32d3fb488e4bfce4abb0031463fda18610bb199
Wataru KUNINO <xbee@dream.jp>
2019-08-04 19:46:14 +0900

    micro:bit

---
+++ micropython/microbit/example01_hello.py
@@ -0,0 +1,6 @@
+from microbit import *
+
+while True:
+	print('Hello, world!')
+	display.show('H e l l o , w o r l d!')
+	display.scroll('Hello, World!')
---
+++ micropython/microbit/example02_txrx.py
@@ -0,0 +1,20 @@
+import radio
+from microbit import *
+
+radio.on()
+print('Ready Tx Rx')
+display.scroll('Rdy')
+while True:
+    tx = None
+    if button_a.was_pressed():
+        tx = 'A'
+    if button_b.was_pressed():
+        tx = 'B'
+    if tx:
+        print('Tx:',tx)
+        radio.send(tx)
+        display.show(tx)
+    recv = radio.receive()
+    if recv != None:
+        print('Rx:',recv)
+        display.scroll(recv)
