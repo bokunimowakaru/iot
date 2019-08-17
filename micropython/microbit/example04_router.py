d27fa59cf378befdb41052058b6cee1648dc998e
bokunimowakaru <xbee@dream.jp>
2019-08-17 12:17:18 +0900

    IoT Router

---
+++ micropython/microbit/example04_router.py
@@ -0,0 +1,20 @@
+import radio
+from microbit import *
+
+radio.on()
+print('Ready Tx Rx')
+display.scroll('Rdy')
+while True:
+	tx = ''
+	line = uart.readline()
+	if line:
+		tx = str(line, 'UTF-8')
+		tx = tx.strip()
+		if len(tx) > 0:
+			print('Tx:',tx)
+			radio.send(tx)
+			display.scroll(tx)
+	recv = radio.receive()
+	if recv != None:
+		print('Rx:',recv)
+		display.scroll(recv)
