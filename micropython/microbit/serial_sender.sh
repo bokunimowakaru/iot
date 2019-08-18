3e898b4b96c8654b3e83022355bf6d1ff3a74b6a
bokunimowakaru <xbee@dream.jp>
2019-08-18 15:08:05 +0900

    Hello!対応

--- micropython/microbit/serial_sender.sh
+++ micropython/microbit/serial_sender.sh
@@ -3,6 +3,11 @@
 # Raspberry Pi用
 # micro:bitのシリアルを送信する
 
+if [ ${#} = 0 ]; then
+	data="Hello!"
+else
+	data=${1}
+fi
 stty -F /dev/ttyACM0 sane igncr -echo 115200
-echo $1 > /dev/ttyACM0
+echo $data > /dev/ttyACM0
 # timeout 1 cat /dev/ttyACM0
