25c8d16c235050fde9b0a861ee0730d9220c637c
Wataru KUNINO <xbee@dream.jp>
2019-08-24 12:39:38 +0900

    書込み機能の追加

--- micropython/nucleo-f767zi/install.sh
+++ micropython/nucleo-f767zi/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "NUCLEO-F767ZI 用 MicroPython のファームウェアを作成します"
+echo "NUCLEO-F767ZI 用 MicroPython のファームウェアを作成し、書き込みます"
 echo "参考文献： https://blog.boochow.com/article/459702269.html"
 echo "ご注意：動作保証はありません"
 echo "Crtl + C で中止します。"
@@ -33,5 +33,30 @@ ls -l firmware.*
 # make -C ports/stm32 MICROPY_HW_ENABLE_ETH_RMII=1 BOARD=NUCLEO_F767ZI deploy-stlink
 
 echo
-echo "バイナリを作成しました。ST-LINK等で書き込んでください。"
+echo "dfu-util をインストールします"
+sleep 5
+sudo apt-get install dfu-util
+
+echo
+echo "NUCLEO-F767ZI へ MicroPython のファームウェアを書き込みます"
+
+while true; do
+	echo "「no」を入力すると終了します"
+	echo -n "yes/no >"
+	read yes
+	if [ $yes = "no" ]; then
+		break;
+	fi
+	dfu-util -a0 -d 0x0483:0xdf11 -D firmware.dfu
+	if [ $? == 0 ]; then
+		echo "書き込みを完了しました。"
+		break;
+	fi
+	echo "失敗しました。"
+	echo
+	echo "再度、ファームウェアを書き込みますか？"
+done
+
+echo
+echo "終了します"
 echo "Done"
