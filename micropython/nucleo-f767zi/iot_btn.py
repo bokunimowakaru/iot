509d1d807e33fea0af6cb4dbaeeb09aebf71c32d
Wataru KUNINO <xbee@dream.jp>
2019-08-24 23:53:04 +0900

    コメント追加

--- micropython/nucleo-f767zi/iot_btn.py
+++ micropython/nucleo-f767zi/iot_btn.py
@@ -5,19 +5,19 @@
 udp_to = '255.255.255.255'                  # UDPブロードキャストアドレス
 udp_port = 1024                             # UDPポート番号
 
-import network
+import network                              # ネットワーク通信ライブラリ
 import socket                               # ソケット通信ライブラリ
 
-pyb.LED(1).on()
-eth = network.Ethernet()
-try:
-    eth.active(True)
-    eth.ifconfig('dhcp')
+pyb.LED(1).on()                             # LED(緑色)を点灯
+eth = network.Ethernet()                    # Ethernetのインスタンスethを生成
+try:                                        # 例外処理の監視を開始
+    eth.active(True)                        # Ethernetを起動
+    eth.ifconfig('dhcp')                    # DHCPクライアントを設定
 except Exception as e:                      # 例外処理発生時
-    pyb.LED(3).on()
+    pyb.LED(3).on()                         # LED(赤色)を点灯
     while True:
         print(e)                            # エラー内容を表示
-        pyb.delay(3000)
+        pyb.delay(3000)                     # 3秒の待ち時間処理
 
 b = 0                                       # ボタン状態を保持する変数bの定義
 sw = pyb.Switch()
@@ -26,10 +26,10 @@ while True:                                 # 繰り返し処理
         pyb.delay(100)                      # 0.1秒間の待ち時間処理
     b = int(not( b ))                       # 変数bの値を論理反転
     if b == 1:                              # b=0:ボタン押下時
-        pyb.LED(2).on()
+        pyb.LED(2).on()                     # LED(青色)を点灯
         udp_s = 'Ping'                      # 変数udp_sへ文字列「Ping」を代入
     else:                                   # b=1:ボタン開放時
-        pyb.LED(2).off()
+        pyb.LED(2).off()                    # LED(青色)を消灯
         udp_s = 'Pong'                      # 変数udp_sへ文字列「Pong」を代入
     print('B1 User', '=', b, udp_s)         # 変数b、udp_sの値を表示
 
@@ -38,9 +38,9 @@ while True:                                 # 繰り返し処理
 
     udp_bytes = (udp_s + '\n').encode()                 # バイト列に変換
 
-    try:                                                # 作成部
+    try:
         sock.sendto(udp_bytes,(udp_to,udp_port))        # UDPブロードキャスト送信
-    except Exception as e:                              # 例外処理発生時
+    except Exception as e:
         print(e)                                        # エラー内容を表示
 
     sock.close()                                        # ソケットの切断
