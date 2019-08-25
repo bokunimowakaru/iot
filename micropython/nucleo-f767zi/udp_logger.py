ab05355ba35cc812d6fb4caed89a6b706e0125ae
Wataru KUNINO <xbee@dream.jp>
2019-08-25 19:28:57 +0900

    コメント追加・LEDの動作追加

--- micropython/nucleo-f767zi/udp_logger.py
+++ micropython/nucleo-f767zi/udp_logger.py
@@ -5,35 +5,41 @@
 # Error ENOMEM や EADDRINUSE が出た場合はハードウェアリセットを実行してください
 #   machine.reset()
 
-port = 1024
+port = 1024                                             # UDPポート番号
 buf_n= 128                                              # 受信バッファ容量(バイト)
 
-import network
-import socket
+import network                                          # ネットワーク通信
+import socket                                           # ソケット通信
 
-pyb.LED(1).on()
-eth = network.Ethernet()
+pyb.LED(1).on()                                         # LED(緑色)を点灯
+eth = network.Ethernet()                                # Ethernet用のethを生成
 try:
-    eth.active(True)
-    eth.ifconfig('dhcp')
+    eth.active(True)                                    # Ethernetを起動
+    eth.ifconfig('dhcp')                                # DHCPクライアントを設定
     sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)# ソケットを作成
     sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)    # オプション
     sock.bind(('',port))                                # ソケットに接続
     print('Listening UDP port', port, '...')            # ポート番号表示
 except Exception as e:                                  # 例外処理発生時
-    pyb.LED(3).on()
+    pyb.LED(3).on()                                     # LED(赤色)を点灯
     while True:
         print(e)                                        # エラー内容を表示
-        pyb.delay(3000)
+        pyb.delay(3000)                                 # 3秒の待ち時間処理
 
 while sock:                                             # 永遠に繰り返す
     udp, udp_from = sock.recvfrom(buf_n)                # UDPパケットを取得
     udp = udp.decode()                                  # UDPデータを文字列に変換
-    pyb.LED(2).on()
+    pyb.LED(2).on()                                     # LED(青色)を点灯
     s=''                                                # 表示用の文字列変数s
     for c in udp:                                       # UDPパケット内
         if ord(c) >= ord(' ') and ord(c) <= ord('~'):   # 表示可能文字
             s += c                                      # 文字列sへ追加
+    if s == 'Ping':                                     # 受信データがPingの時
+        pyb.LED(1).off()                                # LED(緑色)を消灯
+        pyb.LED(3).on()                                 # LED(赤色)を点灯
+    if s == 'Pong':                                     # 受信データがPongの時
+        pyb.LED(1).on()                                 # LED(緑色)を点灯
+        pyb.LED(3).off()                                # LED(赤色)を消灯
     print(udp_from[0] + ', ' + s)                       # 受信データを出力
     pyb.LED(2).off()
 sock.close()                                            # ソケットの切断
