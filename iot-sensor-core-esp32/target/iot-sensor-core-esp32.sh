65ca83f025a4a3b9d28350056ce6197237b4c420
Wataru KUNINO <xbee@dream.jp>
2019-10-19 00:12:03 +0900

    デフォルトUSBポートを設定

--- iot-sensor-core-esp32/target/iot-sensor-core-esp32.sh
+++ iot-sensor-core-esp32/target/iot-sensor-core-esp32.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
+device="/dev/ttyUSB0"
 echo "ESP32へ書き込みます (usage: ${0} port)"
-if [ ${#} -ne 1 ]; then
-	cat README.md
-	echo "ERROR: シリアルポートのデバイスPath(/dev/ttyUSB*)を入力してください"
-	exit
+if [ ${#} -eq 1 ]; then
+        device=${1}
 fi
-if [ ! -e ${1} ]; then
+if [ ! -e ${device} ]; then
 	cat README.md
 	echo "ERROR: 有効なデバイスPath(/dev/ttyUSB*)を入力してください"
 	exit
@@ -18,5 +16,5 @@ if [ ! -e "./esptool.py" ]; then
 	wget https://raw.githubusercontent.com/espressif/esptool/master/LICENSE
 	\mv -f LICENSE esptool_LICENSE.txt
 fi
-./esptool.py --chip esp32 --port ${1} --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 iot-sensor-core-esp32.ino.bin 0x8000 iot-sensor-core-esp32.ino.partitions.bin
+./esptool.py --chip esp32 --port ${device} --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 iot-sensor-core-esp32.ino.bin 0x8000 iot-sensor-core-esp32.ino.partitions.bin
 echo "Done"
