a1ea855ad6edbdf39bf6e496d47b5313f914a7bd
Wataru KUNINO <git@bokunimo.net>
2021-05-09 21:56:03 +0900

    ADC最大値の修正

--- micropython/raspi-pico/example02_temp.py
+++ micropython/raspi-pico/example02_temp.py
@@ -1,4 +1,4 @@
-# Raspberry Pi Pico 内蔵の温度センサの値を取得し、表示する
+# Raspberry Pi の動作確認 温度を測定し表示する
 # Copyright (c) 2021 Wataru KUNINO
 
 from machine import ADC,Pin             # ライブラリmachineのADCを組み込む
@@ -9,7 +9,7 @@ adc = ADC(4)                            # 温度センサ用ADCポートadcを�
 prev = 0                                # 前回の温度値を保持するための変数
 while True:                             # 繰り返し処理
     val = adc.read_u16()                # ADCから値を取得して変数valに代入
-    mv = val * 3300 / 65536             # ADC値を電圧(mV)に変換
+    mv = val * 3300 / 65535             # ADC値を電圧(mV)に変換
     temp = 27 - (mv - 706) / 1.721      # ADC電圧値を温度(℃)に変換
     print('Temperature =',round(temp,1))# 温度値を表示
     if temp > prev:                     # 前回の温度値よりも大きいとき
