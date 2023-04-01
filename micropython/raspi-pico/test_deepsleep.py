3c217e9ddf931a307e459893a35ccb26790d31ab
Wataru KUNINO <git@bokunimo.net>
2023-04-01 11:09:52 +0900

    改良

--- micropython/raspi-pico/test_deepsleep.py
+++ micropython/raspi-pico/test_deepsleep.py
@@ -1,14 +1,27 @@
-# Raspberry Pi Pico の動作確認 Lチカ＋ログ出力表示
-# 0.5秒おきにLEDの点灯と消灯を反転する
-# Copyright (c) 2021 Wataru KUNINO
+###############################################################################
+# Raspberry Pi Pico / Pico W の動作確認 Lチカ＋ログ出力表示
+###############################################################################
+# 5秒おきにスリープ状態を推移します。推移時にLEDの点滅で状態変更を示します。
+#
+# LED表示      Num.  スリープ状態
+# －－－－－    0    sleep (通常)
+# ･－･－･－･－  -    sleep (通常)
+# ･－－－－     1    lightsleep (ライトスリープ)
+# ･－･－･－･－  -    sleep (通常)
+# ･･－－－      2    deepsleep (ディープスリープ)
+# ･－･－･－･－  -    sleep (通常)
+#
+#                                         Copyright (c) 2021-2023 Wataru KUNINO
+###############################################################################
 
 from machine import Pin                 # ライブラリmachineのPinを組み込む
 from machine import deepsleep,lightsleep
 from utime import sleep
 
-sleep_duration = 10                     # スリープ時間（秒）
+sleep_duration = 5                      # スリープ時間（秒）
 
 class sleepmode:
+    num = 3
     mode_sleep = 0
     mode_lightsleep = 1
     mode_deepsleep = 2
@@ -16,6 +29,7 @@ class sleepmode:
 
 def goto_sleep(mode):
     print('Sleep Mode =',sleepmode.mode_name[mode])
+    sleep(0.1)
     if mode == sleepmode.mode_sleep:
         sleep(sleep_duration)           # 待ち時間処理 utime.sleep
     if mode == sleepmode.mode_lightsleep:
@@ -27,7 +41,7 @@ def led_disp(n):                        # LEDの点滅で数字を示す 0～9
     for i in range(1,6):
         led.value(1)
         if n<0 or n>9:
-            sleep(0.2 if i%2 else 0.6) #i%2 ? 0.2 : 0.6
+            sleep(0.2 if i%2 else 0.6)  # i%2 ? 0.2 : 0.6
         elif i <= n%5:
             sleep(0.2 if n<=5 else 0.6) # n<=5 ? 0.2 : 0.6
         else:
@@ -40,15 +54,19 @@ def led_disp(n):                        # LEDの点滅で数字を示す 0～9
         led.value(0)
     sleep(0.6)
 
-mode = sleepmode.mode_deepsleep
 try:
-    led = Pin("LED", Pin.OUT)           # GPIO出力用インスタンスledを生成
+    led = Pin("LED", Pin.OUT)           # GPIO出力用ledを生成(Pico W 用)
 except TypeError:
-    led = Pin(25, Pin.OUT)              # GPIO出力用インスタンスledを生成 
+    led = Pin(25, Pin.OUT)              # GPIO出力用ledを生成(Pico 用)
 
-while True:                             # 繰り返し処理
-    for i in range(3):
-       led_disp(i)                      # スリープ番号0～2をLEDの点滅で出力
-       goto_sleep(i)
-       led_disp(-1)                     # LEDの点滅でスリープ終了を出力
-       sleep(5)
+for mode in range(sleepmode.num):
+   led_disp(mode)                       # スリープ番号0～2をLEDの点滅で出力
+   goto_sleep(mode)
+   led_disp(-1)                         # LEDの点滅でスリープ終了を出力
+   sleep(5)
+
+###############################################################################
+# 以下は実行されない
+mode = sleepmode.mode_deepsleep
+goto_sleep(mode)
+exit()
